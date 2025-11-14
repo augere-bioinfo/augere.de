64301aeff14918eac5abb986260e08b76a6de0c3
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-11-14 21:31:55 +1100

    Added functions to prepare simple design matrices and comparisons.

---
+++ R/processSimpleComparisons.R
@@ -0,0 +1,183 @@
+#' Contrasts for simple comparisons
+#'
+#' Construct R commands to define contrast vectors/matrices for simple comparisons between groups or covariates.
+#'
+#' @param comparisons Character vector specifying the groups to compare or the covariates to test.
+#' Non-character vectors will be coerced into character vectors.
+#' Alternatively, a (possibly named) list of such vectors where each entry represents a different comparison.
+#' @param design.name String containing the variable name of the design matrix.
+#' @param contrast.name String containing the variable name of the contrast vector/matrix.
+#'
+#' @return 
+#' A list containing one entry per comparison, where each entry contains:
+#' \itemize{
+#' \item \code{title}, the title for the comparison.
+#' If \code{comparisons} is named, the corresponding (non-empty) name is used here, otherwise an appropriate title is automatically generated.
+#' \item \code{type}, the type of the comparison. 
+#' This is one of \code{"covariate"} (for covariates), \code{"versus"} (for comparison between two groups or two sets of groups) or \code{"anova"} (for ANOVAs).
+#' \item \code{left} and \code{right}, character vectors specifying the groups on the left (numerator) or right (denominator) of a \dQuote{versus} comparison.
+#' Only present if \code{type = "versus"}.
+#' \item \code{groups}, character vector specifying the groups involved in an ANOVA-like comparison.
+#' Only present if \code{type = "anova"}.
+#' \item \code{covariate}, the name of the covariate being tested.
+#' Only present if \code{type = "covariate"}.
+#' \item \code{commands}, character vector of R commands that produce the desired contrast vector/matrix.
+#' This assumes that the evaluation environment has a design matrix named \code{design.name}.
+#' The newly defined contrast vector/matrix will be stored as \code{contrast.name} in the environment. 
+#' }
+#' 
+#' @details
+#' If a vector in \code{comparisons} is of length 1, the sole entry is assumed to refer to a covariate in the \code{covariates} from \code{\link{processSimpleDesignMatrix}}.
+#' The null hypothesis is that the coefficient of the design matrix with the same name is zero, i.e., the covariate has no effect.
+#'
+#' If a comparison vector is of length 2, the entries represent the names of two groups in the specified \code{groups} factor.
+#' The null hypothesis is that there is no differential expression between the two groups.
+#' The log-fold change is defined as the first group (left) over the second (right). 
+#' 
+#' If a comparison vector is of length 3 or greater, the entries represent the names of groups in the specified \code{groups} factor.
+#' \itemize{
+#' \item If none of the strings are \code{NA}, null hypothesis is that all of the specified levels of \code{groups} are equal.
+#' This is an ANOVA-like contrast where contrasts are formulated with respect to the last level,
+#' i.e., for \code{n} coefficients, \code{n-1} log-fold changes are reported representing the differences relative to the last coefficient.
+#' \item If any of the strings are \code{NA}, this is used to split the vector into two groups of groups.
+#' The null hypothesis is that the averages of the two groups of groups are equal.
+#' The log-fold change is defined as the first average over the second.
+#' }
+#'
+#' @seealso
+#' \code{\link{processSimpleDesignMatrix}}, to generate the corresponding design matrix.
+#'
+#' \code{\link{processCustomContrasts}}, to define more complex custom contrasts.
+#'
+#' @examples
+#' processSimpleComparisons(c("Disease", "Normal"))
+#' processSimpleComparisons("AGE")
+#' processSimpleComparisons(c("Disease", "Treated", "Normal"))
+#' processSimpleComparisons(c("Disease", NA, "Treated", "Normal"))
+#' processSimpleComparisons(list(
+#'     main=c("Disease", "Normal"),
+#'     secondary="AGE"
+#' ))
+#'
+#' @export
+processSimpleComparisons <- function(comparisons, design.name = "design", contrast.name = "con") { 
+    if (!is.list(comparisons)) {
+        comparisons <- list(comparisons)
+    }
+    output <- vector("list", length(comparisons))
+
+    for (i in seq_along(comparisons)) {
+        con <- as.character(comparisons[[i]])
+
+        info <- list(title = NULL)
+        if (!is.null(names(comparisons))) {
+            nm <- names(comparisons)[i]
+            if (nm != "") {
+                info$title <- nm
+            }
+        }
+
+        ngroups <- length(con)
+        if (ngroups == 0L) {
+            stop("vectors in 'comparisons' cannot be empty")
+
+        } else if (ngroups == 1L) {
+            if (is.null(info$title)) {
+                info$title <- sprintf("Effect of increasing `%s`", con)
+            }
+            info$type <- "covariate"
+            info$covariate <- con 
+
+            info$commands <- c(
+                sprintf("%s <- numeric(ncol(%s))", contrast.name, design.name),
+                sprintf("names(%s) <- colnames(%s)", contrast.name, design.name),
+                sprintf("%s[%s] <- 1", contrast.name, deparseToString(paste0("covariate.", con)))
+            )
+
+        } else if (ngroups == 2L) {
+            if (is.null(info$title)) {
+                info$title <- sprintf("Increase in `%s` over `%s`", con[1], con[2])
+            }
+
+            info$type <- "versus"
+            info$left <- con[1]
+            info$right <- con[2]
+
+            info$commands <- c(
+                sprintf("%s <- numeric(ncol(%s))", contrast.name, design.name),
+                sprintf("names(%s) <- colnames(%s)", contrast.name, design.name),
+                sprintf("%s[%s] <- 1", contrast.name, deparseToString(paste0("group.", con[1]))),
+                sprintf("%s[%s] <- -1", contrast.name, deparseToString(paste0("group.", con[2])))
+            )
+
+        } else if (anyNA(con)) {
+            na <- is.na(con)
+            if (sum(na) != 1L) {
+                stop("expected no more than one NA value in the 'comparisons' vector")
+            }
+
+            separator <- which(na)
+            left <- con[1:(separator - 1)]
+            right <- con[(separator + 1):length(separator)]
+
+            info$type <- "versus"
+            info$left <- left
+            info$right <- right
+
+            if (is.null(info$title)) {
+                info$title <- sprintf("Increase in `%s` over %s`", .multiple_group_names(left), .multiple_group_names(right))
+            }
+            info$commands <- c(
+                sprintf("%s <- numeric(ncol(%s))", contrast.name, design.name),
+                sprintf("names(%s) <- colnames(%s)", contrast.name, design.name),
+                sprintf("%s[%s] <- %s", contrast.name, deparseToString(sprintf("group.%s", left)), .multiple_group_coefs(left)),
+                sprintf("%s[%s] <- -%s", contrast.name, deparseToString(sprintf("group.%s", right)), .multiple_group_coefs(right))
+            )
+
+        } else {
+            info$type <- "anova"
+            if (is.null(info$title)) {
+                info$title <- sprintf("One-way ANOVA involving %s", paste(sprintf("`%s`", con), collapse=", "))
+            }
+            info$groups <- con
+
+            commands <- c(
+                sprintf("%s <- matrix(0, ncol(%s), %s)", contrast.name, design.name, ngroups - 1),
+                sprintf("rownames(%s) <- colnames(%s)", contrast.name ,design.name)
+            )
+            for (j in seq_len(ngroups - 1)) {
+                commands <- c(commands, sprintf("%s[%s,%i] <- 1", contrast.name, deparseToString(paste0("group.", con[j])), j))
+            }
+
+            last <- con[ngroups]
+            commands <- c(
+                commands,
+                sprintf("%s[%s,] <- -1", contrast.name, deparseToString(paste0("group.", last))),
+                sprintf("colnames(%s) <- paste0(%s, ' - ', %s)", contrast.name, deparseToString(head(con, -1)), deparseToString(last))
+            )
+            info$commands <- commands
+        }
+
+        output[[i]] <- info
+    }
+
+    output
+}
+
+.multiple_group_names <- function(g) {
+    ngroups <- length(g)
+    if (ngroups == 1) {
+        g
+    } else {
+        sprintf("(%s)/%i", paste(g, collapse=" + "), ngroups)
+    }
+}
+
+.multiple_group_coefs <- function(g){
+    ngroups <- length(g)
+    if (ngroups == 1) {
+        "1"
+    } else {
+        sprintf("1/%i", ngroups)
+    }
+}
---
+++ R/processSimpleDesignMatrix.R
@@ -0,0 +1,78 @@
+#' Create a simple design matrix
+#'
+#' Construct R commands to create a simple design matrix, to be inserted into the generated Rmarkdown report.
+#'
+#' @param groups String specifying the \code{\link[SummarizedExperiment]{colData}} column containing the grouping factor.
+#' @param covariates Character vector specifying the \code{colData} columns containing continuous covariates.
+#' @param block Character vector specifying \code{colData} columns containing additional uninteresting factrors.
+#' @param se.name String containing the variable name of the \link[SummarizedExperiment]{SummarizedExperiment} object.
+#' @param design.name String containing the variable name of the design matrix.
+#'
+#' @return 
+#' Character vector containing R commands that, upon evaluation, create a design matrix in the evaluation environment.
+#'
+#' @author Aaron Lun
+#'
+#' @details
+#' When creating the design matrix, \code{groups}, \code{covariates} and \code{block} are treated as additive factors. 
+#' \itemize{
+#' \item If \code{groups} is specified, the design matrix will not have an intercept.
+#' The first few columns will correspond to the levels of \code{groups} and are named by concatenating \code{groups} with the factor level.
+#' If \code{groups} is not supplied, the design matrix will have an intercept in the first column.
+#' \item If \code{covariates} are specified, they are represented by the columns after the per-group columns (if \code{groups} if supplied) or the intercept (otherwise).
+#' \item All remaining columns will correspond to the various levels of the \code{block} factors.
+#' }
+#'
+#' Syntactically invalid column names and levels can be used for all arguments.
+#'
+#' The design matrix is expected to be of full column rank, e.g., \code{groups} is not confounded with elements of \code{block}.
+#'
+#' @examples
+#' cat(processSimpleDesignMatrix("my_groups", "age", "batch", "se", "design"), sep="\n")
+#'
+#' @seealso
+#' \code{\link{processCustomDesignMatrix}}, for creating more complex design matrices.
+#'
+#' \code{\link{processSimpleComparisons}}, to create contrasts for this design matrix.
+#'
+#' @export
+#' @import augere.core
+processSimpleDesignMatrix <- function(groups, covariates, block, se.name, design.name) {
+    cmds <- c(
+        "model.data <- list()",
+        sprintf("cd <- SummarizedExperiment::colData(%s)", se.name)
+    )
+
+    has.groups <- !is.null(groups)
+    if (has.groups) {
+        cmds <- c(cmds, sprintf("model.data$group. <- factor(cd[,%s])", deparseToString(groups)))
+        terms <- "group."
+    } else {
+        terms <- character(0)
+    }
+
+    if (!is.null(covariates)) {
+        for (x in seq_along(covariates)) {
+            name <- paste0("covariate.", covariates[x])
+            cmds <- c(cmds, sprintf("model.data$%s <- as.numeric(cd[,%s])", name, deparseToString(covariates[x])))
+            terms <- c(terms, name)
+        }
+    }
+
+    if (!is.null(block)) {
+        for (b in seq_along(block)) {
+            name <- paste0("block.", block[b], ".")
+            cmds <- c(cmds, sprintf("model.data$%s <- factor(cd[,%s])", name, deparseToString(block[b])))
+            terms <- c(terms, name)
+        }
+    }
+
+    formula <- paste(terms, collapse=" + ")
+    if (has.groups) {
+        formula <- paste("~ 0 +", formula)
+    } else {
+        formula <- paste("~", formula)
+    }
+
+    c(cmds, sprintf("%s <- model.matrix(%s, data=model.data)", design.name, formula))
+}
