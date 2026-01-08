ee0b6bbb8054afc2408083f343a9750c7eacadc8
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2026-01-08 18:43:49 +1100

    Pre-emptively implement minor fixes from BioC review.
    Also added a bunch of biocViews.

---
+++ R/augere.de-package.R
@@ -0,0 +1,8 @@
+#' augere.de
+#'
+#' Implement differential expression analyses in the augere framework.
+#' Pipeline functions execute DE analyses with edgeR or voom,
+#' also generating a self-contained Rmarkdown report to reproduce each analysis.
+#'
+#' @name augere.de-package
+NULL
